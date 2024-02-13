024353a774ed946805a64947c73d40e0b0e39f68
Viacheslav Trus <viacheslav.trus@dev4you.pl>
2024-02-13 22:23:47 +0100

    last

--- main.py
+++ main.py
@@ -181,13 +181,13 @@ def create_new_invoice(products):
                     "Miejscowosc": product["e134f8360b17a18963ca6ea8cfaa7e0b156b7f91_locality"],
                 }
             }
-            #status, new_invoice = create_invoice(invoice_model)
-            #if status == 0:
-            print("send")
-            invoice_nr, price = get_invoice_by_id(1195697)
-            send_mail(product["person_id"]["email"][0]["value"], 1195697, invoice_nr, price, product['10cb2dd06a7a60d9d9e19bd3819a6569ffb208c1'], product['5073621992b2b327ea4ca4733833c97af8aadc4e'],product['199cc63d9c8efe4d49249d9a7e97318015d8cb10'], product['4f14897cef15702d7cf7583bea70e89bafa36646'])
-            print("done")
-            #pipedrive_service.mark_as_sent(product["id"])
+            status, new_invoice = create_invoice(invoice_model)
+            if status == 0:
+                print("send")
+                invoice_nr, price = get_invoice_by_id(1195697)
+                send_mail(product["person_id"]["email"][0]["value"], 1195697, invoice_nr, price, product['10cb2dd06a7a60d9d9e19bd3819a6569ffb208c1'], product['5073621992b2b327ea4ca4733833c97af8aadc4e'],product['199cc63d9c8efe4d49249d9a7e97318015d8cb10'], product['4f14897cef15702d7cf7583bea70e89bafa36646'])
+                print("done")
+                pipedrive_service.mark_as_sent(product["id"])
 
 while True:
     try:
